a910270f720e3cbcf33de841608b7782797b05b5
Kameer Hosein <kameer-hosien@outlook.com>
2024-12-15 16:31:18 -0400

    Initial Commit

---
+++ video_playerTests/video_playerTests.swift
@@ -0,0 +1,17 @@
+//
+//  video_playerTests.swift
+//  video_playerTests
+//
+//  Created by Kameer Hosein on 12/15/24.
+//
+
+import Testing
+@testable import video_player
+
+struct video_playerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
