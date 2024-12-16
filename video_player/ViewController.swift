1898549f405037e611ada3c4e259802ae2568433
Kameer Hosein <kameer-hosien@outlook.com>
2024-12-15 21:00:18 -0400

    first commit

--- video_player/ViewController.swift
+++ video_player/ViewController.swift
@@ -6,7 +6,8 @@
 //
 
 import UIKit
-
+import AVKit
+import AVFoundation
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -14,6 +15,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        playVideo()
+    }
+        private func playVideo() {
+            guard let path = Bundle.main.path(forResource: "American Truck Simulator - Western Star® 57X - SCS Software (1080p, h264, youtube)", ofType: "mp4") else{
+                debugPrint("Could not find video")
+                return
+            }
+            
+            let player = AVPlayer(url: URL(fileURLWithPath: path))
+            let playerController=AVPlayerViewController()
+            playerController.player=player
+            present(playerController, animated: true){
+                player.play()
+            }
+        }
+    }
 
-}
 
